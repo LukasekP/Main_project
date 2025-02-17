839fe7879be1dcd96bf3d8642653dd74934eea4a
Lukas <lukas.prazak@student.ossp.cz>
2025-02-17 14:03:27 +0100

    Přidán uživatel Lukas, backend formuláře na přihlášení

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ def base():
     return render_template("base.html")
 
 @app.route('/addBook', methods=['GET', 'POST'])
-def link():
+def addbook():
     if request.method == 'POST':
         name = request.form['name']
         description = request.form['description']
         radio = request.form['radio']
         return render_template("base.html", name=name, description=description, radio=radio)
     return render_template("addBook.html")
+
+@app.route('/odkaz', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return render_template("user.html", username=username, password=password)
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
