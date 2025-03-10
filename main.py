6d388de489c2fb5d41acc2f105c33fe8dba6f169
Lukas <lukas.prazak@student.ossp.cz>
2025-03-10 10:25:55 +0100

    Pripojení databaze

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-from flask import Flask, render_template, request
+from app import app, login
+from app.db import create_db
+from os import path
 
-app = Flask(__name__)
+if __name__ == '__main__':
+    print(app.config["DATABASE"])
+    if not path.exists(app.config["DATABASE"]):
+        create_db()
+        print("inicializace database")
 
-@app.route('/')
-def base():
-    return render_template("base.html")
-
-@app.route('/addBook', methods=['GET', 'POST'])
-def addbook():
-    if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
-        radio = request.form['radio']
-        return render_template("base.html", name=name, description=description, radio=radio)
-    return render_template("addBook.html")
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        return render_template("login.html", username=username, password=password)
-    return render_template("login.html")
+app.register_blueprint(login.bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
