1f3bc64d7ea48efd1948afcf1fb88a3a5a88042e
Lukas <lukas.prazak@student.ossp.cz>
2025-03-31 09:40:34 +0200

    pridani komentare do souboru __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="../static", template_folder="../templates")
-app.config["SECRET_KEY"] = "dev"
-app.config["DATABASE"] =    "database.sqlite"
+
+"""Konfigurační nastavení Flask aplikace
+
+Attributes:
+    SECRET_KEY: Tajný klíč pro zabezpečení session cookies a dalších bezpečnostních funkcí
+    DATABASE: Cesta k SQLite databázovému souboru
+"""
+app.config["SECRET_KEY"] = "dev"  # V produkci nahradit skutečným tajným klíčem
+app.config["DATABASE"] = "database.sqlite"
+
 
 @app.route("/")
 def index():
+    """Obsluha kořenové route aplikace
+
+    Zobrazuje domovskou stránku aplikace na základě šablony index.html
+
+    Returns:
+        str: Vyrenderovaná HTML šablona index.html
+    """
     return render_template("index.html")
