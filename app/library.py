4239239f69674738a5a9c1a2329f7a3804f36344
Lukas <lukas.prazak@student.ossp.cz>
2025-03-31 09:44:39 +0200

    pridani komentare do souboru library.py

--- app/library.py
+++ app/library.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template, request, Blueprint, flash, session, redirect, url_for
 from app.login import login_required
-bp = Blueprint('library', __name__, url_prefix='/library', template_folder='../templates', static_folder='../static')
+
+"""
+Blueprint pro knihovní část aplikace
+
+Attributes:
+    name: Název blueprintu ('library')
+    url_prefix: Základní cesta pro všechny routy ('/library')
+    template_folder: Cesta k šablonám (o úroveň výš v adresáři templates)
+    static_folder: Cesta ke statickým souborům (o úroveň výš v adresáři static)
+"""
+bp = Blueprint('library', __name__,
+               url_prefix='/library',
+               template_folder='../templates',
+               static_folder='../static')
+
+
 @bp.route('/')
 @login_required
 def index():
+    """Zobrazí hlavní stránku knihovny
+
+    Požaduje přihlášení uživatele (@login_required).
+    Vrací vyrenderovanou šablonu library.html.
+
+    Returns:
+        str: Vyrenderovaná HTML šablona library.html
+    """
     return render_template("library.html")
